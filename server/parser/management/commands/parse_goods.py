3a3913ec4f2fbdc1e7bd6c3a64a98acb6c7a518f
shyzik93 <shyzik93@mail.ru>
2025-04-16 02:37:04 +0300

    improve parser, readme

--- server/parser/management/commands/parse_goods.py
+++ server/parser/management/commands/parse_goods.py
@@ -1,5 +1,8 @@
+import math
+import re
+
+import lxml.html as html
 import requests
-from django.conf import settings
 from django.core.management.base import BaseCommand
 from parser.models import GoodRule
 
@@ -8,10 +11,22 @@ class Command(BaseCommand):
     help = 'Парсит товары по правилам, сохранённым в БД'
 
     def handle(self, *args, **options):
-        for good_rule in GoodRule.objects.all():
-            response = requests.get(good_rule['url'])
+        price_re = re.compile(r'\d+')
+        prices_sum = 0
+        count_goods = 0
+        for count_goods, good_rule in enumerate(GoodRule.objects.all()):
+            response = requests.get(good_rule.url)
             if response.status_code == 200:
                 response_text = response.text
-                price = 0
-                title = good_rule['title']
-                self.stdout.write(f'{title}: {price}')
+                document = html.document_fromstring(response_text)
+                price_elements = document.xpath(good_rule.xpath)
+                if price_elements:
+                    raw_price = price_elements[0].text_content()
+                    price = int(price_re.findall(raw_price.strip())[0])
+                    title = good_rule.title
+                    prices_sum += price
+                    self.stdout.write(f'{title}: {price}')
+
+        self.stdout.write('')
+        average_price = (prices_sum / count_goods) if count_goods > 0 else 0 
+        self.stdout.write(f'Средняя цена: {average_price}')
