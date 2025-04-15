0753cebf1b8fc5f21516cc05afb7f21f09fbed13
shyzik93 <shyzik93@mail.ru>
2025-04-15 22:59:30 +0300

    delete db

--- server/server/settings.py
+++ server/server/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / '.sqlite3',
+        'NAME': BASE_DIR / '.sqlite3.db',
     }
 }
 
